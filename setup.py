63c0d5fe5a4c5734d758a2f280a4c7be73e3f560
pRusya <agat.nk@gmail.com>
2016-07-29 00:08:33 +0300

    update action_hooks; add libs/

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='mafpub',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='mafpub for OpenShift',
+      author='Ruslan Pozin',
+      author_email='ruslan.pozin@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=[
+        'Django==1.8.12',
+        'Pillow',
+        'requests'
+      ],
+      dependency_links=[
+        'https://pypi.python.org/simple/django/'
+      ],
      )
