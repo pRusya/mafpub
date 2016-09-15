290e1c3fd49b53f165be43435a1caaa486369172
pRusya <agat.nk@gmail.com>
2016-09-15 13:22:25 +0300

    update setup, requirements; remove django-pagedown static files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='mafpub',
       version='1.0',
-      description='mafpub for OpenShift',
+      description='mafpub for OpenShift v2',
       author='Ruslan Pozin',
       author_email='ruslan.pozin@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
@@ -12,7 +12,7 @@ setup(name='mafpub',
         'Django==1.8.12',
         'Pillow==3.1.1',
         'requests',
-        'django-summernote=0.8.5',
+        'django-summernote==0.8.5',
       ],
       dependency_links=[
         'https://pypi.python.org/simple/django/'
